0d81ce000109fb35c89c9dd06dd6ddb89672ad51
Vu Minh Duc <vu.minh.duc@framgia.com>
2015-07-08 18:27:33 +0700

    Admin manage user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root "static_pages#home"
   get "help" => "static_pages#help"
   get "about" => "static_pages#about"
-
-  devise_for :users
 
   namespace :admin do
     root "dashboards#index"
