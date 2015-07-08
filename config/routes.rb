f07931987288ae7d8f73030f02979e2e3b98bb2f
Do Tuan Minh <do.tuan.minh@gmail.com>
2015-07-08 16:21:46 +0700

    sign-in_sign-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "static_pages#home"
-  
   get "help" => "static_pages#help"
   get "about" => "static_pages#about"
+
+  devise_for :users
 end
