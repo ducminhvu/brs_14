86d8b635edbdde04c2cabcd8121d24a0c16cf567
Vu Minh Duc <vu.minh.duc@framgia.com>
2015-07-10 12:49:15 +0700

    Manage request: Delete request

--- app/controllers/admin/requests_controller.rb
+++ app/controllers/admin/requests_controller.rb
@@ -1,12 +1,21 @@
 class Admin::RequestsController < ApplicationController
   before_action :required_admin
-  before_action :set_request, only: [:show, :destroy]
+  before_action :set_request, only: [:show, :destroy, :update]
 
   def index
     @requests = Request.list_request.paginate page: params[:page]
   end
 
   def show
+  end
+
+  def update
+    @request.bought = true
+    if @request.save
+      respond_to do |format|
+        format.js
+      end
+    end
   end
 
   def destroy
