cacfcae41371040cec6c1b9639df36d4519aaeec
michalplachyFramgia <michal.plachy@framgia.com>
2015-07-13 08:10:14 +0700

    Filter Book

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-50.times do |n|
+20.times do |n|
   name  = Faker::Name.name
   email = "test-#{n+1}@brs.com"
   password = "password"
@@ -6,11 +6,24 @@
     password_confirmation: password)
 end
 
-50.times do |n|
+20.times do |n|
   name = Faker::Lorem.sentence 1
   description = Faker::Lorem.sentence 6
   author = Faker::Name.name
   bought = rand(2) == 1 ? true : false
   Request.create!(book_name: name, description: description,
     author: author, user_id: n+2, bought: bought)
+end
+
+5.times do |n|
+  name = Faker::Lorem.sentence 1
+  Category.create! name: name
+end
+
+20.times do |n|
+  title  = Faker::Name.name
+  description = "test-#{n+1}@brs.com"
+  category_id = 1
+  Book.create!(title: title, description: description, category_id: category_id,
+    number_page: 12)
 end
