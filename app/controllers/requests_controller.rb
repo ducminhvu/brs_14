c64025125df599af88b2fc918c6f998906ee300d
Vu Minh Duc <vu.minh.duc@framgia.com>
2015-07-13 17:13:32 +0700

    Add request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,14 @@
 class RequestsController < ApplicationController
   def index
-    @requests = Request.list_request_of(current_user).sort_request.paginate page: params[:page],
-      per_page: Settings.requests_per_page
+    @requests = Request.list_request_of(current_user).sort_request
+      .paginate page: params[:page], per_page: Settings.requests_per_page
+    @request = Request.new
+  end
+
+  def create
+    @request = Request.new request_params
+    @request.save
+    redirect_to :back
   end
 
   def destroy
@@ -11,5 +18,10 @@ class RequestsController < ApplicationController
         format.js
       end
     end
+  end
+
+  private
+  def request_params
+    params.require(:request).permit :book_name, :author, :description, :user_id
   end
 end
