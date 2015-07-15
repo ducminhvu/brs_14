f13122fb9e5c631ce88ab87dd55adf2dfcc304b2
michalplachyFramgia <michal.plachy@framgia.com>
2015-07-15 08:57:52 +0700

    Activities follow

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,15 @@
 class Relationship < ActiveRecord::Base
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+  after_create :activity_follow
+  after_destroy :activity_unfollow
+
+  private
+  def activity_follow
+    Activity.create! action: "#{self.follower.name} has followed #{self.followed.name}", user_id: self.follower_id
+  end
+
+  def activity_unfollow
+    Activity.create! action: "#{self.follower.name} has unfollowed #{self.followed.name}", user_id: self.follower_id
+  end
 end
