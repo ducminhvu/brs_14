6d4100a3fe8b936faddc8c0cadc8361c1244e210
Do Tuan Minh <do.tuan.minh@gmail.com>
2015-07-15 10:10:48 +0700

    reading-status

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -2,4 +2,18 @@ class ReadingsController < ApplicationController
   def index
     @readings = current_user.reading_books
   end
+
+  def create
+    @book = Book.find params[:book_id]
+    @reading = Reading.find_or_initialize_by book_id: @book.id, user_id: current_user.id
+    @reading.status = params[:status]
+    respond_to do |format|
+      if @reading.save
+        format.html {redirect_to @book}
+        format.js
+      else
+        redirect_to :back
+      end
+    end
+  end
 end
